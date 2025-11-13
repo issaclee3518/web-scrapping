1b59c08fcf4374536b8da336da46442a0b69ce5f
issaclee6320 <issaclee6320@gmail.com>
2025-11-13 14:40:52 +0900

    select tag complete

--- myenv/web_scrap_test/test.py
+++ myenv/web_scrap_test/test.py
@@ -1,7 +1,29 @@
 import requests
-
+from bs4 import BeautifulSoup
 url = "https://www.google.com"
 
 response = requests.get(url)
-
+html='''
+<nav class="navbar">
+    <div class="container">
+        <a href="/" class="navbar-brand">My Website</a>
+    </div>
+</nav>
+<main>
+    <h1>Welcome to my website</h1>
+    <div>This is a simple website built with HTML and CSS.</div>
+    <a href="https://www.google.com">Google</a>
+</main>
+<footer>
+    <div>Copyright 2025 My Website</div>
+</footer>
+'''
+bs = BeautifulSoup(html,"html.parser")
 print(response.status_code)
+
+div_tags = bs.select('div')
+print(div_tags)
+for div_tag in div_tags:
+    print(div_tag.get_text())
+print("--------------------------------")
+print(bs)
